10f5d43fb129445a1075ca840722826ec569f985
gadgetcontrolsystem <gadgetcontrolsystem@gmail.com>
2016-12-17 16:40:12 +0600

    Initial commit

---
+++ src/main/java/kz/gcs/rest/service/UserService.java
@@ -0,0 +1,27 @@
+package kz.gcs.rest.service;
+
+import java.util.List;
+
+import kz.gcs.rest.model.User;
+
+
+
+public interface UserService {
+	
+	User findById(long id);
+	
+	User findByName(String name);
+	
+	void saveUser(User user);
+	
+	void updateUser(User user);
+	
+	void deleteUserById(long id);
+
+	List<User> findAllUsers(); 
+	
+	void deleteAllUsers();
+	
+	public boolean isUserExist(User user);
+	
+}
